dc217d4f7d868c0bd2296a41e398617f7117b290
santos-victory <santhoshvictorysoftwares@gmailcom>
2022-02-07 11:03:32 +0530

    adding jar file

---
+++ Neww.java
@@ -0,0 +1,12 @@
+public class Neww{
+public static void main(String[] args)
+{
+for(int i=1;i<5;i++)
+{
+System.out.println("code builded successfully .."+ i);
+}
+System.out.println("edited by santos");
+System.out.println("just for checkin for test");
+
+}
+}
