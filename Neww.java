e61bde5972c1fefa1cae2fa60451b7b8753fc80c
santos-victory <santhoshvictorysoftwares@gmailcom>
2022-02-07 11:12:49 +0530

    updating files

--- Neww.java
+++ Neww.java
@@ -6,7 +6,7 @@ for(int i=1;i<5;i++)
 System.out.println("code builded successfully .."+ i);
 }
 System.out.println("edited by santos");
-System.out.println("just for checkin for test");
+System.out.println("just checkin for test");
 
 }
 }
